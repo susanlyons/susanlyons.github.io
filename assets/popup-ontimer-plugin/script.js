bff21debe915d9d9bc95d6c7518fe3b41f47b5fb
susanlyons <41521543+susanlyons@users.noreply.github.com>
2021-08-22 11:58:33 -0400

    Add files via upload

--- assets/popup-ontimer-plugin/script.js
+++ assets/popup-ontimer-plugin/script.js
@@ -1,2 +1,3 @@
 
-(function(a){a(document).ready(function(){a("html").hasClass("is-builder")||a(".mbr-popup[data-on-timer-delay]").each(function(d,b){var c=a(b).attr("data-on-timer-delay");setTimeout(function(){a(b).modal("show")},1E3*c)})})})(jQuery);
+(function(){document.addEventListener("DOMContentLoaded",function(){document.querySelector("html").classList.contains("is-builder")||document.querySelectorAll(".mbr-popup[data-on-timer-delay]").forEach(function(b){var c=b.getAttribute("data-on-timer-delay"),a;"undefined"!==typeof bootstrap?("undefined"!==typeof bootstrap.Modal.getInstance&&(a=bootstrap.Modal.getInstance(b)),a||(a=new bootstrap.Modal(b),{keyboard:!1}),setTimeout(function(){a.show()},1E3*c)):"undefined"!==typeof jQuery&&setTimeout(function(){$(b).modal({keyboard:!1})},
+1E3*c);setTimeout(function(){a.show()},1E3*c)})})})();
